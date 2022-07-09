2fa46b9b25c9a632c543d251ae49c32fca1df0e5
Guetso <legalloisprovensal@gmail.com>
2022-07-09 18:00:57 +0200

    test tailwind

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,10 +1,15 @@
 <!-- Please remove this file from your project -->
 <template>
   <div
-    class="flex justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+    class="flex flex-col justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
   >
-    <div class="flex justify-center pt-8 sm:pt-0"><ApicalLogo /></div>
+    <div class="flex justify-center sm:pt-0">
+      <ApicalLogo />
+    </div>
+    <h1 class="flex justify-center sm:pt-0 text-2xl font-bold">APICACAL</h1>
+    <h2 class="flex justify-center mt-4 text-2xl">Ça va chier des bûches !</h2>
   </div>
+  
 </template>
 
 <script>
