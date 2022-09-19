d14e11fa8af626903da9947cc866663c0c3d20cd
Guetso <legalloisprovensal@gmail.com>
2022-09-19 11:42:54 +0200

    Updates

---
+++ _nuxt/static/1663580570/legals/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fapical\u002F_nuxt\u002Fstatic\u002F1663580570",layout:"default",error:a,serverRendered:true,routePath:"\u002Flegals",config:{_app:{basePath:"\u002Fapical\u002F",assetsPath:"\u002Fapical\u002F_nuxt\u002F",cdnURL:a}}}}(null));
